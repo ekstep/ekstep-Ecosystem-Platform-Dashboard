f89dfc1d61aaf1c1b15e7dd32ba5326c5e3cd840
Gaurav Singhania <grvsinghania@gmail.com>
2015-07-20 11:38:55 +0530

    Copied line chart as step 1 for multi y axis | <Singhania>

--- src/kibana/components/vislib/visualizations/vis_types.js
+++ src/kibana/components/vislib/visualizations/vis_types.js
@@ -14,7 +14,8 @@ define(function (require) {
       pie: Private(require('components/vislib/visualizations/pie_chart')),
       line: Private(require('components/vislib/visualizations/line_chart')),
       area: Private(require('components/vislib/visualizations/area_chart')),
-      tile_map: Private(require('components/vislib/visualizations/tile_map'))
+      tile_map: Private(require('components/vislib/visualizations/tile_map')),
+      multi_y: Private(require('components/vislib/visualizations/line_chart'))
     };
   };
 
