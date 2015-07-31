elastic/kibana

Sorting fields based on X-Axis labels

src/kibana/components/agg_types/buckets/terms.js
@@ -112,7 +112,7 @@ define(function (require) {
               if (!orderBy && prevOrderBy === INIT) {
                 // abort until we get the responseValueAggs
                 if (!$scope.responseValueAggs) return;
-                params.orderBy = (_.first($scope.responseValueAggs) || { id: 'custom' }).id;
+                params.orderBy = (_.first($scope.responseValueAggs) || { id: 'custom' } || {id: 'x_axis'}).id;
                 return;
               }
 
@@ -123,7 +123,7 @@ define(function (require) {
               if (!orderBy || orderBy !== 'custom') {
                 params.orderAgg = null;
                 // ensure that orderBy is set to a valid agg
-                if (!_.find($scope.responseValueAggs, { id: orderBy })) {
+                if (!_.find($scope.responseValueAggs, { id: orderBy }) && orderBy !== '_term') {
                   params.orderBy = null;
                 }
                 return;
@@ -136,6 +136,11 @@ define(function (require) {
             var vis = agg.vis;
             var dir = agg.params.order.val;
             var order = output.params.order = {};
+
+            if (agg.params.orderBy === '_term') {
+              order._term = dir;
+              return;
+            }
 
             var orderAgg = agg.params.orderAgg || vis.aggs.getResponseAggById(agg.params.orderBy);
 

ekstep/ekstep-Ecosystem-Platform-Dashboard

src/kibana/components/agg_types/buckets/terms.js
@@ -112,7 +112,7 @@ define(function (require) {
               if (!orderBy && prevOrderBy === INIT) {
                 // abort until we get the responseValueAggs
                 if (!$scope.responseValueAggs) return;
-                params.orderBy = (_.first($scope.responseValueAggs) || { id: 'custom' }).id;
+                params.orderBy = (_.first($scope.responseValueAggs) || { id: 'custom' } || {id: '_term'}).id;
                 return;
               }
 
@@ -123,7 +123,7 @@ define(function (require) {
               if (!orderBy || orderBy !== 'custom') {
                 params.orderAgg = null;
                 // ensure that orderBy is set to a valid agg
-                if (!_.find($scope.responseValueAggs, { id: orderBy })) {
+                if (!_.find($scope.responseValueAggs, { id: orderBy }) && orderBy !== '_term') {
                   params.orderBy = null;
                 }
                 return;
@@ -136,6 +136,11 @@ define(function (require) {
             var vis = agg.vis;
             var dir = agg.params.order.val;
             var order = output.params.order = {};
+
+            if (agg.params.orderBy === '_term') {
+              order._term = dir;
+              return;
+            }
 
             var orderAgg = agg.params.orderAgg || vis.aggs.getResponseAggById(agg.params.orderBy);
 