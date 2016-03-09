11f84d1148eafe197f2581e7e11bb8bcd9f88948
Kiran G <kirang20@gmail.com>
2016-03-09 13:44:49 +0530

    Revert "Community portal SSO API integration"
    This reverts commit f22aa69e9cbb87594ea57c76c0ed68b9c96f887a.

--- src/server/app.js
+++ src/server/app.js
@@ -7,58 +7,7 @@ var appHeaders = require('./lib/appHeaders');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var compression = require('compression');
-var session = require( 'express-session' );
 var config = require('./config');
-var passport = require('passport' );
-var UniqueTokenStrategy = require('passport-unique-token').Strategy;
-var SHA256 = require("crypto-js/sha256");
-var request = require('request');
-var url = require('url') ;
-var querystring = require('querystring');
-
-// var SECRET = '5f4dcc3b5aa765d61d8327deb882cf99';
-// var COMMUNITY_PORTAL_ENDPOINT = "http://sandbox-community.ekstep.org";
-// var COMMUNITY_PORTAL_SSO_REQUESTT = "/index.php?option=com_api&app=sso&resource=sso&format=raw";
-// var COMMUNITY_PORTAL_LOGIN_ENDPOINT = "http://sandbox-community.ekstep.org/index.php?option=com_ekcontent&view=dashboard&layout=operational";
-
-var SECRET = '{{ community_portal_sso_secret }}'
-var COMMUNITY_PORTAL_ENDPOINT = "{{ community_portal_endpoint }}";
-var COMMUNITY_PORTAL_SSO_REQUESTT = "{{ community_portal_sso_requestt }}";
-var COMMUNITY_PORTAL_LOGIN_ENDPOINT = "{{ community_portal_login_endpoint }}"
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-passport.use(new UniqueTokenStrategy(
-  function (token, done) {
-    if(token){
-      var sha256Token = SHA256(token, SECRET);
-      return request.post(
-          COMMUNITY_PORTAL_ENDPOINT + COMMUNITY_PORTAL_SSO_REQUESTT + '&hash=' + sha256Token + '&client_id:' + null,
-          null,
-          function (error, response, body) {
-              if (!error &&
-                body.responseCode === "OK" &&
-                body.params.status === "successful") {
-                console.log('auth successful');
-                return done(null, true);
-              } else {
-                console.log('auth failed, response: ' + JSON.stringify(response));
-                return done(null, false);
-              }
-          }
-      );
-    } else {
-      console.log('No token, failing authentication');
-      return done(null, false);
-    }
-  }
-));
 
 var routes = require('./routes/index');
 var proxy = require('./routes/proxy');
@@ -88,28 +37,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(compression());
-app.use(session({
-  secret: 'random secret'
-}));
-app.use( passport.initialize());
-app.use( passport.session());
-app.use(ensureAuthenticated);
 app.use(express.static(config.public_folder));
-if (config.external_plugins_folder)
-  app.use('/plugins', express.static(config.external_plugins_folder));
-
-app.get( '/login',
-  passport.authenticate( 'token', {
-    successRedirect: '/',
-    failureRedirect: COMMUNITY_PORTAL_LOGIN_ENDPOINT
-}));
-
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/login');
-});
+if (config.external_plugins_folder) app.use('/plugins', express.static(config.external_plugins_folder));
 
 app.use('/', routes);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -117,6 +49,8 @@ app.use(function (req, res, next) {
   err.status = 404;
   next(err);
 });
+
+// error handlers
 
 // development error handler
 // will print stacktrace
@@ -140,20 +74,5 @@ app.use(function (err, req, res, next) {
   });
 });
 
-
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  //throw new Error(req.isAuthenticated());
-  if (req.path === "/login" || req.path.indexOf("/auth") > -1  || req.path.indexOf('/oauth2callback') > -1 ) {
-    return next();
-  }
-  var queryString = url.parse(req.url,true).query;
-  var login_url = "/login";
-  if(querystring.stringify(queryString) !== "" ||
-    querystring.stringify(queryString) != undefined) {
-    login_url = "/login?"+ querystring.stringify(queryString);
-  }
-  res.redirect(login_url);
-}
 
 module.exports = app;
